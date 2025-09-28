11591e586db4ec67d552427a9e3516d2a2d5f089
Maik-Biazi <maikwilliam4553@gmail.com>
2025-09-28 00:19:02 -0300

    refatorando aplicação

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ echo "[entrypoint] aguardando DB..."
 # espera o banco ficar pronto
 until nc -z $PG_HOST $PG_PORT; do
   echo "Aguardando banco $PG_HOST:$PG_PORT..."
-  sleep 2
+  sleep 6
 done
 
 echo "[entrypoint] knex migrate:latest"
